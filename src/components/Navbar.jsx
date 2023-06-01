b2ef1fb629d3449d2a6dfa8a12c0be960b2efc21
Shaurya10101 <shauryagupta8046@gmail.com>
2023-06-01 08:20:29 +0530

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,6 +11,7 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
+import logo from '\src\assets\react.svg'
 
 const pages = ['Exercise', 'Food', 'Blogs'];
 const settings = ['Profile', 'Logout'];
